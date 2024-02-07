9ce77657c68562d1c8f2b7a375bbb4bf21cecbe7
UnforgettableThunder <SagittariusDew@gmail.com>
2024-02-07 11:42:03 +0200

    repo added

---
+++ src/main/java/com/example/gittest/repository/TestRepository.java
@@ -0,0 +1,9 @@
+package com.example.gittest.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRepository {
+    private List<String> list = new ArrayList<>();
+
+}
